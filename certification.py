384d8af9bdaa3a5f53f11c33c8eef236e9e8d357
MU <m.iouriev@gmail.com>
2024-01-25 22:53:08 +0700

    Код начал отделять элементы которые больше 3х, но выводит их посивольно

--- certification.py
+++ certification.py
@@ -2,19 +2,16 @@ n =  3#int(input("fuf: "))
 masive_1 = []
 masive_2 = []
 i = 0
-minimal = "asd"
 
 while i < n:
     masive_1 += [str(input())]
     i += 1
-    
-print(masive_1)
 
 i = 0
-for i in masive_1:
-    if masive_1[i] < minimal:
+
+while i < len(masive_1):
+    if len(masive_1[i]) <= 3:
         masive_2 += masive_1[i]
     i += 1
-
-print(masive_1)
-print(masive_2)
+print(*masive_2)
+    
