8d3a335a7a9730e74efaf0ef5f17b37e024f7288
MU <m.iouriev@gmail.com>
2024-01-27 22:45:58 +0700

    изменил блок-схему, переиминовал переменые в коде и доптсал README

--- certification.py
+++ certification.py
@@ -1,17 +1,17 @@
 size =  3 #int(input("fuf: "))
-masive_1 = [] #["1234", "1567", "-2", "computer science"]
-masive_2 = []
+start_masive = [] #["1234", "1567", "-2", "computer science"]
+finish_masive = []
 i = 0
 
 while i < size:
-    masive_1.append(str(input()))
+    start_masive.append(str(input()))
     i += 1
 
 i = 0
 
-while i < len(masive_1):
-    if len(masive_1[i]) <= 3:
-        masive_2.append(masive_1[i])
+while i < len(start_masive):
+    if len(start_masive[i]) <= 3:
+        finish_masive.append(start_masive[i])
     
     i += 1
-print(masive_2)
+print(finish_masive)
