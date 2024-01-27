20878f1882ae3ed33d1c206c308cacbb704487cd
MU <m.iouriev@gmail.com>
2024-01-27 21:16:50 +0700

    Код работает

--- certification.py
+++ certification.py
@@ -1,17 +1,17 @@
-n =  3#int(input("fuf: "))
-masive_1 = []
+n =  3 #int(input("fuf: "))
+masive_1 = [] #["1234", "1567", "-2", "computer science"]
 masive_2 = []
 i = 0
 
 while i < n:
-    masive_1 += [str(input())]
+    masive_1.append(str(input()))
     i += 1
 
 i = 0
 
 while i < len(masive_1):
     if len(masive_1[i]) <= 3:
-        masive_2 += masive_1[i]
-    i += 1
-print(*masive_2)
+        masive_2.append(masive_1[i])
     
+    i += 1
+print(masive_2)
